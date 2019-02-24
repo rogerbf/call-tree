0c6cf2e33d99580f418a9543ac19d997fa311f34
rogerbf <roger@bergfa.lk>
2019-02-24 21:21:06 +0100

    includes example

---
+++ examples/includes.js
@@ -0,0 +1,16 @@
+const { includes } = require(`../`)
+
+const e = () => {}
+const f = () => {}
+
+const tree = {
+  a: {
+    b: () => {},
+  },
+  c: {
+    d: [ { e } ],
+  },
+}
+
+console.log(includes(tree, e)) // true
+console.log(includes(tree, f)) // false
